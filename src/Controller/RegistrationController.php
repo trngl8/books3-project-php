80004a77252600bee0d485dd1b0957883d1a9abf
mvchn <movchan@gmail.com>
2021-10-03 10:52:01 +0300

    fix encoder

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -16,7 +16,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mime\Address;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
 
 class RegistrationController extends AbstractController
@@ -34,7 +34,7 @@ class RegistrationController extends AbstractController
         $this->em = $em;
     }
 
-    public function register(Request $request, UserPasswordHasherInterface $passwordEncoder): Response
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
     {
         $registration = new Registration();
         $form = $this->createForm(RegistrationFormType::class, $registration);
