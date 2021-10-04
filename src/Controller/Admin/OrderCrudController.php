c7ad2f59a3cad22a14d928d6be33a22db00ba16b
mvchn <movchan@gmail.com>
2021-10-04 21:26:11 +0300

    fields fix

--- src/Controller/Admin/OrderCrudController.php
+++ src/Controller/Admin/OrderCrudController.php
@@ -3,12 +3,17 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Order;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 
 class OrderCrudController extends AbstractCrudController
 {
@@ -19,12 +24,26 @@ class OrderCrudController extends AbstractCrudController
 
     public function configureFields(string $pageName): iterable
     {
-        return [
-            //IdField::new('id'),
-            TextField::new('name'),
-            EmailField::new('email'),
-            CollectionField::new('cards'),
-            ArrayField::new('options'),
-        ];
+        yield IntegerField::new('id', 'ID')->hideOnForm();
+        yield TextField::new('name');
+        yield EmailField::new('email');
+        yield AssociationField::new('cards');
+
+//        return [
+//            //IdField::new('id'),
+//            TextField::new('name'),
+//            EmailField::new('email'),
+//            CollectionField::new('cards'),
+//            ArrayField::new('options'),
+//        ];
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->add(Crud::PAGE_INDEX, Action::DETAIL)
+            //->add(Crud::PAGE_INDEX, Action::DELETE)
+            ->add(Crud::PAGE_EDIT, Action::SAVE_AND_ADD_ANOTHER)
+        ;
     }
 }
