3f6feb6dfc3b77d38e978ceb8f30d3ee1303b872
mvchn <movchan@gmail.com>
2021-10-09 17:25:04 +0300

    add new abilities for orders

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Order;
+use App\Repository\OrderRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,6 +17,51 @@ class OrderController extends AbstractController
     public function checkout(Order $order): Response
     {
         return $this->render('order/checkout.html.twig', [
+            'order' => $order,
+        ]);
+    }
+
+    /**
+     * @Route("/orders", name="orders_list")
+     */
+    public function list(OrderRepository $repo): Response
+    {
+        $orders = $repo->findAll();
+
+        return $this->render('order/list.html.twig', [
+            'orders' => $orders,
+        ]);
+    }
+
+    /**
+     * @Route("/orders/{id}/show", name="orders_show")
+     */
+    public function show(Order $order): Response
+    {
+        return $this->render('order/show.html.twig', [
+            'order' => $order,
+        ]);
+    }
+
+    /**
+     * @Route("/orders/{id}/remove", name="orders_remove")
+     */
+    public function remove(Order $order, Request $request): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        $submittedToken = $request->request->get('token');
+
+        if ($this->isCsrfTokenValid('remove', $submittedToken)) {
+            $this->getDoctrine()->getManager()->remove($order);
+            $this->getDoctrine()->getManager()->flush();
+
+            $this->addFlash('success', 'Order removed');
+
+            return $this->redirectToRoute('orders_list');
+        }
+
+        return $this->render('order/remove.html.twig', [
             'order' => $order,
         ]);
     }
