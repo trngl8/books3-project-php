06d59194fb495606d1617ccdc209276f6686d07b
mvchn <movchan@gmail.com>
2021-10-10 18:00:09 +0300

    fix frontend

--- assets/app.js
+++ assets/app.js
@@ -11,3 +11,19 @@ import './styles/global.scss';
 
 // start the Stimulus application
 import './bootstrap';
+
+
+const $ = require('jquery');
+
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
+
+// or you can include specific pieces
+// require('bootstrap/js/dist/tooltip');
+require('bootstrap/js/dist/popover');
+
+
+// $(document).ready(function() {
+//     $('[data-toggle="popover"]').popover();
+// });
--- webpack.config.js
+++ webpack.config.js
@@ -17,10 +17,15 @@ Encore
     /*
      * ENTRY CONFIG
      *
+     * Add 1 entry for each "page" of your app
+     * (including one that's included on every page - e.g. "app")
+     *
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    //.addEntry('page1', './assets/page1.js')
+    //.addEntry('page2', './assets/page2.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
@@ -61,15 +66,16 @@ Encore
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
 
-    // uncomment if you use React
-    //.enableReactPreset()
-
     // uncomment to get integrity="..." attributes on your script & link tags
     // requires WebpackEncoreBundle 1.4 or higher
     //.enableIntegrityHashes(Encore.isProduction())
 
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
+
+    // uncomment if you use API Platform Admin (composer require api-admin)
+    //.enableReactPreset()
+    //.addEntry('admin', './assets/admin.js')
 ;
 
 module.exports = Encore.getWebpackConfig();
