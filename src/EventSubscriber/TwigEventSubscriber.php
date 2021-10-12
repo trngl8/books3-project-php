a240563d6b1e186f2a7626b2a8d96f8d20ce018d
mvchn <movchan@gmail.com>
2021-10-12 19:59:22 +0300

    fix bell

--- src/EventSubscriber/TwigEventSubscriber.php
+++ src/EventSubscriber/TwigEventSubscriber.php
@@ -7,24 +7,31 @@ use App\Repository\ProfileRepository;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\ControllerEvent;
 use Twig\Environment;
+use Symfony\Component\Security\Core\Security;
 
 class TwigEventSubscriber implements EventSubscriberInterface
 {
     private $twig;
     private $profileRepo;
     private $inboxRepo;
+    private $security;
 
-    public function __construct(Environment $twig, ProfileRepository $profileRepo, InboxRepository $inboxRepo)
+
+    public function __construct(Environment $twig, ProfileRepository $profileRepo, InboxRepository $inboxRepo, Security $security)
     {
         $this->twig = $twig;
         $this->profileRepo = $profileRepo;
         $this->inboxRepo = $inboxRepo;
+        $this->security = $security;
     }
 
     public function onControllerEvent(ControllerEvent $event)
     {
         $this->twig->addGlobal('profiles', $this->profileRepo->findAll());
-        //$this->twig->addGlobal('messages', $this->inboxRepo->findAll());
+        $user = $this->security->getUser();
+        if($user) {
+            $this->twig->addGlobal('global_messages', $this->inboxRepo->findBy(['address' => $user->getUserIdentifier()]));
+        }
     }
 
     public static function getSubscribedEvents()
