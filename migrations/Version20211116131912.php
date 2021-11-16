ec9ba366090cbc154fb44acf7c4759c41c1b298a
mvchn <movchan@gmail.com>
2021-11-16 15:29:28 +0200

    Close https://github.com/mvchn/booking/issues/222

---
+++ migrations/Version20211116131912.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20211116131912 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('UPDATE card SET active = true where active is NULL');
+
+    }
+
+    public function down(Schema $schema): void
+    {
+    }
+}
